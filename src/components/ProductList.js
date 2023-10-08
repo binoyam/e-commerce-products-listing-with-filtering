33aa8cf1e974b44126f82e4ac1f38c660c9e0979
binoyam <binyam.techan01@gmail.com>
2023-10-08 04:21:30 +0300

    rendering products

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,7 +2,28 @@ import React from "react";
 import Product from "./Product";
 
 function ProductList({ products }) {
-    // console.log(typeof(products))
+  //   console.log(products);
+
+  function countSameValues(products, key) {
+    const valueCount = {};
+    products.forEach((obj) => {
+      const value = obj[key];
+
+      if (valueCount[value]) {
+        valueCount[value]++;
+      } else {
+        valueCount[value] = 1;
+      }
+    });
+    return valueCount;
+  }
+  const valueCount = countSameValues(products, "is_available");
+  const valueCount1 = countSameValues(products, "category");
+  const valueCount2 = countSameValues(products, "color");
+
+  console.log(valueCount);
+  console.log(valueCount1);
+  console.log(valueCount2);
   return (
     <>
       {products.map((product, id) => (
