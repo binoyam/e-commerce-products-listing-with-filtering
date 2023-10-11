7463772b8ae2ef2f7798c6f7c947e1633134bc37
binoyam <binyam.techan01@gmail.com>
2023-10-11 05:04:20 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import Data from "./productData.json";
 import Header from "./components/Header";
 import ProductList from "./components/ProductList";
 import Product from "./components/Product";
-// import { Routes, Route } from "react-router-dom";
 
 function App() {
-  // console.log(typeof(Data))
   const [selectedProduct, setSelectedProduct] = useState(null);
 
- 
   const [filteredProducts, setFilteredProducts] = useState(Data);
   return (
     <div className="app">
